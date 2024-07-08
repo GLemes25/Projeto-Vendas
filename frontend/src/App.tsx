9c65aea5471200471524102949db5ea2b5270277
GLemes25 <gabriellemes924@gmail.com>
2024-07-08 11:03:53 -0400

    Graficos Estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import BarCharts from "components/Charts/BarChart";
+import DonutCharts from "components/Charts/DonutChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -5,14 +7,28 @@ import NavBar from "components/NavBar";
 const App = () => {
   return (
     <>
-    <NavBar />
-    <div className="container"> 
-      <h1 className="text-primary">Olá mundo!</h1>
-    <DataTable/>
-    </div>
-     <Footer/>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashborad de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarCharts />
+          </div>
+          <div className="col-sm-6">
+            <h5>Todas as Vendas</h5>
+            <DonutCharts />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary"> Todas vendas</h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
     </>
-  ); 
-}
+  );
+};
 
 export default App;
